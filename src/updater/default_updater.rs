1c572f84639104502e1653af7af350c4ba896f50
ityeri <ityeri9@gmail.com>
2026-04-11 00:18:45 +0900

    edit: Separate mass, radius calculation to each method

--- src/updater/default_updater.rs
+++ src/updater/default_updater.rs
@@ -20,13 +20,13 @@ impl DefaultUpdater {
     pub fn default_setting() -> Self {
         Self {
             base_neighbor_radius: 3.0,
-            neighbor_radius: 0.1,
+            neighbor_radius: 0.5,
             neighbor_space_ratio: 0.2,
-            mass_amplification: 1.0,
-            min_node_mass: 0.1,
-            neighbor_edge_elasticity: 0.01,
-            non_neighbor_repulsive_force: 100.0,
-            non_neighbor_distance_softning: 0.1,
+            mass_amplification: 0.25,
+            min_node_mass: 1.0,
+            neighbor_edge_elasticity: 20.0,
+            non_neighbor_repulsive_force: 2000.0,
+            non_neighbor_distance_softning: 0.01,
             attenuation_rate: 800.0,
         }
     }
@@ -39,22 +39,21 @@ impl DefaultUpdater {
     ) -> PhysicalNode<K> {
         let node = graph.nodes.get(&node_key).unwrap();
 
-        let mass = self.min_node_mass + node.incomings.len() as f32 * self.mass_amplification;
+        let mass = self.get_mass(&node);
+        let this_radius = self.get_neighbor_radius(&node);
 
         let force = graph
             .nodes
             .values()
             .filter(|&node| node.key != node_key)
             .map(|other_node| -> Vec2 {
-                let other_node_mass =
-                    self.min_node_mass + other_node.incomings.len() as f32 * self.mass_amplification;
+                let other_node_mass = self.get_mass(&other_node);
 
-                let neighbor_radius = (node.incomings.len() * other_node.incomings.len()) as f32
-                    * self.neighbor_radius;
+                let edge_length = this_radius + self.get_neighbor_radius(&other_node);
 
-                let min_distance = neighbor_radius * (1.0 - self.neighbor_space_ratio / 2.0)
+                let min_distance = edge_length * (1.0 - self.neighbor_space_ratio / 2.0)
                     + self.base_neighbor_radius;
-                let max_distance = neighbor_radius * (1.0 + self.neighbor_space_ratio / 2.0)
+                let max_distance = edge_length * (1.0 + self.neighbor_space_ratio / 2.0)
                     + self.base_neighbor_radius;
 
                 let diff = other_node.position - node.position;
@@ -79,7 +78,7 @@ impl DefaultUpdater {
                     self.non_neighbor_repulsive_force
                         * -diff.normalize_or_zero()
                         * (mass * other_node_mass)
-                        / (diff.length().powi(2) + 0.1)
+                        / (diff.length().powi(2) + self.non_neighbor_distance_softning)
                 }
             })
             .sum::<Vec2>();
@@ -94,6 +93,13 @@ impl DefaultUpdater {
             position: 2.0 * node.position - node.prev_position + acc * dt.powi(2),
             prev_position: node.position,
         }
+    }
+
+    fn get_mass<K: NodeKey>(&self, node: &PhysicalNode<K>) -> f32 {
+        self.min_node_mass + node.incomings.len() as f32 * self.mass_amplification
+    }
+    fn get_neighbor_radius<K: NodeKey>(&self, node: &PhysicalNode<K>) -> f32 {
+        node.incomings.len() as f32 * self.neighbor_radius
     }
 }
 
