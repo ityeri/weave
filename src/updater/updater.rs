a5a6dbc112b3aab4cca0ed5b584aefdc90b708ba
ityeri <ityeri9@gmail.com>
2026-04-25 01:52:23 +0900

    edit: Made Updater.update func gets ref of the graph struct

--- src/updater/updater.rs
+++ src/updater/updater.rs
@@ -1,5 +1,5 @@
 use crate::{NodeKey, PhysicalGraph};
 
 pub trait Updater {
-    fn update<K: NodeKey>(&self, graph: PhysicalGraph<K>, dt: f32) -> PhysicalGraph<K>;
+    fn update<K: NodeKey>(&self, graph: &PhysicalGraph<K>, dt: f32) -> PhysicalGraph<K>;
 }
