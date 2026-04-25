6561d1ce048b2e0cf0a7be1369f20edc3ed01365
ityeri <ityeri9@gmail.com>
2026-04-25 16:15:06 +0900

    format: Import syntax

--- examples/node_physics.rs
+++ examples/node_physics.rs
@@ -1,16 +1,15 @@
-use petgraph::visit::EdgeRef;
 use glam::{DVec2, Vec2};
 use macroquad::shapes::{draw_circle, draw_line};
 use macroquad::{
     color,
     input::{
-        KeyCode, MouseButton, is_key_pressed, is_mouse_button_down, mouse_position,
-        mouse_wheel,
+        KeyCode, MouseButton, is_key_pressed, is_mouse_button_down, mouse_position, mouse_wheel,
     },
     text::draw_text,
     time::{get_fps, get_frame_time},
     window::{Conf, clear_background, next_frame, screen_height, screen_width},
 };
+use petgraph::visit::EdgeRef;
 use petgraph::visit::IntoEdgeReferences;
 use petgraph::{
     Directed, Direction,
@@ -143,7 +142,7 @@ async fn main() {
         }
 
         if update_running {
-            let updated_graph = updater.update(physical_graph, fixed_dt);
+            let updated_graph = updater.update(&physical_graph, fixed_dt);
 
             for (node_index, node) in updated_graph.nodes {
                 graph[node_index].position = node.position;
@@ -187,7 +186,9 @@ async fn main() {
                 graph[node_index].position.x as f64,
                 graph[node_index].position.y as f64,
             ));
-            let degrees = graph.edges_directed(node_index, Direction::Incoming).count();
+            let degrees = graph
+                .edges_directed(node_index, Direction::Incoming)
+                .count();
             let diameter = 1.0 + degrees as f64 * 0.1;
 
             let projcted_radius = if 1.0 < adaptor.project_scale(diameter * 0.5) {
